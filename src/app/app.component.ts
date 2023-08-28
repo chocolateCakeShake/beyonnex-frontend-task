e9263e61f709114933b99abc0913adbd624dad9d
chocolateCakeShake <abdullahworkboi@gmail.com>
2023-08-28 22:09:58 +0500

    added some adjustable controls for testing purposes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'beyonnex-frontend-task';
+
+  maxTemp: number = 100;
+  minTemp: number = 0;
+  currentTemp: number = 22;
 }
